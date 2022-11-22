f5cbbbb2263e1467d760591ace68360559d78577
gaboliveiradev <g38789138@gmail.com>
2022-11-22 20:05:44 -0300

    TESTE

--- App/View/Modules/Dashboard/MenuInicial.php
+++ App/View/Modules/Dashboard/MenuInicial.php
@@ -6,11 +6,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include "./View/Includes/Bootstrap/css_config.php" ?>
     <title>Dashboard - Logado como <?= $_SESSION['admin_logged']->nome ?></title>
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'Topping');
+            data.addColumn('number', 'Slices');
+            data.addRows([
+              ['Feminino', <?= 10 ?>],
+              ['Masculino', <?= 20 ?>]
+            ]);
+          
+            var options = {'title':'Pessoas x Sexo', 'width':400, 'height':300};
+          
+            // Instantiate and draw our chart, passing in some options.
+            var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+            chart.draw(data, options);
+        }
+    </script>
 </head>
 <body>
     <header>
         <?php include "./View/Includes/header.php" ?>
     </header>
+    <section>
+        <div id="chart_div"></div>
+    </section>
 
     <?php include './View/Includes/Bootstrap/js_config.php' ?>
 </body>
--- App/View/Modules/VagaEmprego/ListaVagaEmprego.php
+++ App/View/Modules/VagaEmprego/ListaVagaEmprego.php
@@ -12,6 +12,37 @@
         <?php include "./View/Includes/header.php" ?>
     </header>
     <h1>Lista de Vaga de Empregos</h1>
+    <section>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td colspan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+    </section>
 
     <?php include "./View/Includes/Bootstrap/js_config.php" ?>
 </body>
